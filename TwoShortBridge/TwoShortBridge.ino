d32283a8f68dbbef00e3042f171fa1cbb6cf1a12
Federico Carminati <federico.carminati@gmail.com>
2023-12-22 16:34:13 -0700

    Two Short Bridges

--- TwoShortBridge/TwoShortBridge.ino
+++ TwoShortBridge/TwoShortBridge.ino
@@ -1,75 +1,132 @@
+//
+// Sketch for two bridges
+//
+// Pin configuration
+//
+// Bridge 1
+// A0 analog input for sensor 1R
+// A1 analog input for sensor 1L
+// D2 front red lights
+// D3 bridge lights
+// D4 read red lights
+// D5 control for sensor 1R
+// D6 control for sensor 1L
+//
+// Bridge 2
+// A2 analog input for sensor 2R
+// A3 analog input for sensor 2L
+// D7 front red lights
+// D9 bridge lights
+// D8 rear red lights
+// D9 control for sensor 2R
+// D10 control for sensor 2L
+//
+ 
+// Bridge 1
+int red_front_b1 = 2;
+int lights_b1 = 3;
+int red_back_b1 = 4;
+int sensR_b1 = 5;
+int sensL_b1 = 6;
+int brightness_b1 = 0;
+unsigned long fademils_b1 = millis();
+unsigned long blinkred_b1 = millis();
+int highlow_b1 = 0;
+int befR_b1 = 0;
+int befL_b1 = 0;
+int aftR_b1 = 0;
+int aftL_b1 = 0;
+int fade_b1 = 1;
 
-int led = 9;           // the PWM pin the LED is attached to
-int frontred = 8;
-int rearred = 10;
-int brightness = 0;    // how bright the LED is
-int fadeAmount = 5;    // how many points to fade the LED by
-int sens1 = 6;
-int sens2 = 7;
-int vccs1 = LOW;
-int vccs2 = LOW;
-unsigned long fademils = millis();
-unsigned long blinkred = millis();
-int highlow = 0;
-int bef1 = 0;
-int aft1 = 0;
-bool ss1 = FALSE:
-bool ss2 = FALSE;
+// Bridge 2
+int red_front_b2 = 7;
+int lights_b2 = 9;
+int red_back_b2 = 8;
+int sensR_b2 = 9;
+int sensL_b2 = 10;
+int brightness_b2 = 0;
+unsigned long fademils_b2 = millis();
+unsigned long blinkred_b2 = millis();
+int highlow_b2 = 0;
+int befR_b2 = 0;
+int befL_b2 = 0;
+int aftR_b2 = 0;
+int aftL_b2 = 0;
+int fade_b2 = 1;
+
+const int fadeAmount = 5;    // how many points to fade the LED by
+
+bool ss1 = false;
+bool ss2 = false;
 
 // the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT); //LED on Model B
-  pinMode(frontred, OUTPUT); // front red lights
-  pinMode(rearred, OUTPUT); // front red lights
-  pinMode(sens1, OUTPUT); // front red lights
-  pinMode(sens2, OUTPUT); // front red lights
+  // initialize pins
+  // Bridge 1
+  pinMode(red_front_b1, OUTPUT);
+  pinMode(lights_b1, OUTPUT);
+  pinMode(red_back_b1, OUTPUT);
+  pinMode(sensR_b1, OUTPUT);
+  pinMode(sensL_b1, OUTPUT);
+
+  // Bridge 2
+  pinMode(red_front_b2, OUTPUT);
+  pinMode(lights_b2, OUTPUT);
+  pinMode(red_back_b2, OUTPUT);
+  pinMode(sensR_b2, OUTPUT);
+  pinMode(sensL_b2, OUTPUT);
+
   pinMode(13, OUTPUT); //LED on Model A  or Pro
-  Serial.begin(9600); 
+  Serial.begin(57600); 
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(sens1,LOW);
-  bef1 = analogRead(A0);
-  digitalWrite(sens1,HIGH);
-  delay(10);
-  aft1 = analogRead(A0);
-  Serial.println(aft1-bef1);
-  if(aft1-bef1 < 100)
+  digitalWrite(sensR_b1,HIGH);
+  befR_b1  = analogRead(A0);
+  befR_b1 += analogRead(A0);
+  befR_b1 += analogRead(A0);
+  befR_b1 /= 3.;  
+  delay(50);
+  digitalWrite(sensR_b1,LOW);
+  aftR_b1  = analogRead(A0);
+  aftR_b1 += analogRead(A0);
+  aftR_b1 += analogRead(A0);
+  aftR_b1 /= 3.;
+  if(aftR_b1-befR_b1 > 100) Serial.println(aftR_b1-befR_b1);
+  if(aftR_b1-befR_b1 < 100)
     digitalWrite(13,HIGH);
    else 
     digitalWrite(13,LOW);   
 
-  digitalWrite(sens2,LOW);
-  bef2 = analogRead(A1);
-  digitalWrite(sens1,HIGH);
+  digitalWrite(sensL_b1,LOW);
+  befL_b1 = analogRead(A1);
+  digitalWrite(sensL_b1,HIGH);
   delay(10);
-  aft2 = analogRead(A1);
-  Serial.println(aft1-bef1);
-  if(aft2-bef2 < 100)
+  aftL_b1 = analogRead(A1);
+//  Serial.println(aftL_b1-befL_b1);
+  if(aftL_b1-befL_b1 < 100)
     digitalWrite(13,HIGH);
    else 
     digitalWrite(13,LOW);   
 
-  if(ss1 || ss2
-  if(millis()-fademils > 50) {
-    fademils = millis();
-    // set the brightness of pin 9:
-    analogWrite(led, brightness);
+  if(millis()-fademils_b1 > 50) {
+    fademils_b1 = millis();
+    // set the brightness bridge lights
+    analogWrite(lights_b1, brightness_b1);
 
     // change the brightness for next time through the loop:
-    brightness = brightness + fadeAmount;
+    brightness_b1 = brightness_b1 + fade_b1 * fadeAmount;
 
     // reverse the direction of the fading at the ends of the fade:
-    if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
+    if (brightness_b1 <= 0 || brightness_b1 >= 255) {
+    fade_b1 = -fade_b1;
     }
-    if(millis() - blinkred > 500) {
-      blinkred = millis();
-      highlow = 1-highlow;
-      digitalWrite(frontred,highlow);
-      digitalWrite(rearred,1-highlow);
+    if(millis() - blinkred_b1 > 500) {
+      blinkred_b1 = millis();
+      highlow_b1 = 1-highlow_b1;
+      digitalWrite(red_front_b1,highlow_b1);
+      digitalWrite(red_back_b1,1-highlow_b1);
     }
   }
 }
