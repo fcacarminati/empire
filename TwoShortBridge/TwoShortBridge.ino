471dbce2201911ac4278e4dee56d18fa3a02cd3d
Federico Carminati <federico.carminati@gmail.com>
2023-12-22 21:29:38 -0700

    First version with only one bridge

--- TwoShortBridge/TwoShortBridge.ino
+++ TwoShortBridge/TwoShortBridge.ino
@@ -4,57 +4,61 @@
 // Pin configuration
 //
 // Bridge 1
-// A0 analog input for sensor 1R
-// A1 analog input for sensor 1L
-// D2 front red lights
-// D3 bridge lights
-// D4 read red lights
-// D5 control for sensor 1R
-// D6 control for sensor 1L
+// D2  driver right red lights
+// D3  driver bridge lights
+// D4  driver read red lights
+// D5  digital input for sensor 1R
+// D6  digital input for sensor 1L
 //
 // Bridge 2
-// A2 analog input for sensor 2R
-// A3 analog input for sensor 2L
-// D7 front red lights
-// D9 bridge lights
-// D8 rear red lights
-// D9 control for sensor 2R
-// D10 control for sensor 2L
+// D7  driver right red lights
+// D9  driver bridge lights
+// D8  driver left red lights
+// D9  digital input for sensor 2R
+// D10 digital input dor sensor 2L
 //
  
 // Bridge 1
-int red_front_b1 = 2;
-int lights_b1 = 3;
-int red_back_b1 = 4;
-int sensR_b1 = 5;
-int sensL_b1 = 6;
+int redR_b1 = 2;   // pin for right red lights
+int lights_b1 = 3; // pin white lights
+int redL_b1 = 4;   // pin for left red lights
+int sensR_b1 = 5;  // right sensor
+int sensL_b1 = 6;  // left sensor
 int brightness_b1 = 0;
 unsigned long fademils_b1 = millis();
 unsigned long blinkred_b1 = millis();
 int highlow_b1 = 0;
-int befR_b1 = 0;
-int befL_b1 = 0;
-int aftR_b1 = 0;
-int aftL_b1 = 0;
-int fade_b1 = 1;
+short curR_b1 = 0;    // current state of right sensor 
+short curL_b1 = 0;    // current state of left sensor
+short preR_b1 = 0;    // previous state of right sensor
+short preL_b1 = 0;    // previous state of left sensor
+short stateR_b1 = 0;  // logical state of right side
+                      // 0 off; 1 set; 2 cleared; 3 on
+short stateL_b1 = 0;  // logical state of left side
+short fade_b1 = 0;    // fading direction
 
 // Bridge 2
-int red_front_b2 = 7;
+int redR_b2 = 7;
 int lights_b2 = 9;
-int red_back_b2 = 8;
+int redL_b2 = 8;
 int sensR_b2 = 9;
 int sensL_b2 = 10;
 int brightness_b2 = 0;
 unsigned long fademils_b2 = millis();
 unsigned long blinkred_b2 = millis();
 int highlow_b2 = 0;
-int befR_b2 = 0;
-int befL_b2 = 0;
-int aftR_b2 = 0;
-int aftL_b2 = 0;
-int fade_b2 = 1;
+short curR_b2 = 0;
+short curL_b2 = 0;
+short preR_b2 = 0;
+short preL_b2 = 0;
+short stateR_b2 = 0;  // logical state of right side
+                      // 0 off; 1 set; 2 cleared; 3 on
+short stateL_b2 = 0;  // logical state of left side
+short fade_b2 = 0;
 
 const int fadeAmount = 5;    // how many points to fade the LED by
+
+enum state {kOff, kSet, kClear, kOn};
 
 bool ss1 = false;
 bool ss2 = false;
@@ -63,69 +67,78 @@ bool ss2 = false;
 void setup() {                
   // initialize pins
   // Bridge 1
-  pinMode(red_front_b1, OUTPUT);
+  pinMode(redR_b1, OUTPUT);
   pinMode(lights_b1, OUTPUT);
-  pinMode(red_back_b1, OUTPUT);
-  pinMode(sensR_b1, OUTPUT);
-  pinMode(sensL_b1, OUTPUT);
+  pinMode(redL_b1, OUTPUT);
+  pinMode(sensR_b1, INPUT);
+  pinMode(sensL_b1, INPUT);
 
   // Bridge 2
-  pinMode(red_front_b2, OUTPUT);
+  pinMode(redR_b2, OUTPUT);
   pinMode(lights_b2, OUTPUT);
-  pinMode(red_back_b2, OUTPUT);
-  pinMode(sensR_b2, OUTPUT);
-  pinMode(sensL_b2, OUTPUT);
+  pinMode(redL_b2, OUTPUT);
+  pinMode(sensR_b2, INPUT);
+  pinMode(sensL_b2, INPUT);
 
-  pinMode(13, OUTPUT); //LED on Model A  or Pro
   Serial.begin(57600); 
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  const short nrep = 5;
-  befR_b1 = 0;
-  for(short i=0; i<nrep; ++i) 
-    befR_b1 += analogRead(A0);
-  befR_b1 /= nrep; 
-  delay(50);
-  aftR_b1 = 0;
-  for(short i=0; i<nrep; ++i) 
-    aftR_b1 += analogRead(A0);
-  aftR_b1 /= nrep; 
-  Serial.println(befR_b1-aftR_b1);
-  if(aftR_b1-befR_b1 < 100)
-    digitalWrite(13,HIGH);
-   else 
-    digitalWrite(13,LOW);   
+  curR_b1 = digitalRead(sensR_b1);
+  stateR_b1 = curR_b1 + 2*preR_b1;
+  preR_b1 = curR_b1;
+  
+  curL_b1 = digitalRead(sensL_b1);
+  stateL_b1 = curL_b1 + 2*preL_b1;
+  preL_b1 = curL_b1;
 
-  digitalWrite(sensL_b1,LOW);
-  befL_b1 = analogRead(A1);
-  digitalWrite(sensL_b1,HIGH);
-  delay(10);
-  aftL_b1 = analogRead(A1);
-//  Serial.println(aftL_b1-befL_b1);
-  if(aftL_b1-befL_b1 < 100)
-    digitalWrite(13,HIGH);
-   else 
-    digitalWrite(13,LOW);   
+// Is the train entering?
+  if((stateR_b1 == kSet && stateL_b1 != kOn) ||
+     (stateR_b1 != kOn  && stateL_b1 == kSet))
+    fade_b1 = 1;
 
-  if(millis()-fademils_b1 > 50) {
-    fademils_b1 = millis();
-    // set the brightness bridge lights
-    analogWrite(lights_b1, brightness_b1);
+// Is the train exiting?
+  else
+    if((stateR_b1 == kClear && stateL_b1 == kOff) ||
+     (stateR_b1 == kOff   && stateL_b1 == kClear))
+    fade_b1 = -1;
 
-    // change the brightness for next time through the loop:
-    brightness_b1 = brightness_b1 + fade_b1 * fadeAmount;
+// The train is either in or out
+    else
+      fade_b1 = 0;
 
-    // reverse the direction of the fading at the ends of the fade:
-    if (brightness_b1 <= 0 || brightness_b1 >= 255) {
-    fade_b1 = -fade_b1;
+  if(fade_b1) {
+  
+    if(millis()-fademils_b1 > 50) {
+      fademils_b1 = millis();
+
+      // change the brightness for next time through the loop:
+      brightness_b1 = brightness_b1 + fade_b1 * fadeAmount;
+
+      // set the brightness bridge lights
+      analogWrite(lights_b1, brightness_b1);
+      
+      // stop fading if we have reached max or min
+      if (brightness_b1 <= 0) {
+	brightness_b1 = 0;
+	fade_b1 = 0;
+      } else if(brightness_b1 >= 255) {
+	brightness_b1 = 255;
+	fade_b1 = 0;
+      }
     }
+  }
+  if(brightness_b1 > 0) {
     if(millis() - blinkred_b1 > 500) {
       blinkred_b1 = millis();
       highlow_b1 = 1-highlow_b1;
-      digitalWrite(red_front_b1,highlow_b1);
-      digitalWrite(red_back_b1,1-highlow_b1);
+      digitalWrite(redR_b1,highlow_b1);
+      digitalWrite(redL_b1,1-highlow_b1);
+    } else {
+      digitalWrite(redR_b1,LOW);
+      digitalWrite(redL_b1,LOW);
     }
   }
 }
+
