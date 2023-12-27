3a7ac596d6ca399334f37544c23bcf148cc524f7
Federico Carminati <federico.carminati@gmail.com>
2023-12-27 16:45:58 -0700

    FIxing

--- TwoShortBridge/TwoShortBridge.ino
+++ TwoShortBridge/TwoShortBridge.ino
@@ -36,6 +36,8 @@ short stateR_b1 = 0;  // logical state of right side
                       // 0 off; 1 set; 2 cleared; 3 on
 short stateL_b1 = 0;  // logical state of left side
 short fade_b1 = 0;    // fading direction
+bool trainInL_b1 = false;
+bool trainInR_b1 = false;
 
 // Bridge 2
 int redR_b2 = 7;
@@ -55,8 +57,10 @@ short stateR_b2 = 0;  // logical state of right side
                       // 0 off; 1 set; 2 cleared; 3 on
 short stateL_b2 = 0;  // logical state of left side
 short fade_b2 = 0;
+bool trainInL_b2 = false;
+bool trainInR_b2 = false;
 
-const int fadeAmount = 5;    // how many points to fade the LED by
+const int fadeAmount = 1;    // how many points to fade the LED by
 
 enum state {kOn, kSet, kClear, kOff};
 
@@ -69,22 +73,22 @@ void setup() {
   pinMode(redL_b1, OUTPUT);
   pinMode(sensR_b1, INPUT);
   pinMode(sensL_b1, INPUT);
-
+  
   digitalWrite(redR_b1, HIGH);
   digitalWrite(lights_b1, HIGH);
   digitalWrite(redL_b1, HIGH);
-
+  
   // Bridge 2
   pinMode(redR_b2, OUTPUT);
   pinMode(lights_b2, OUTPUT);
   pinMode(redL_b2, OUTPUT);
   pinMode(sensR_b2, INPUT);
   pinMode(sensL_b2, INPUT);
-
+  
   digitalWrite(redR_b2, HIGH);
   digitalWrite(lights_b2, HIGH);
   digitalWrite(redL_b2, HIGH);
-
+  
   Serial.begin(57600); 
 }
 
@@ -93,60 +97,56 @@ void loop() {
   curR_b1 = digitalRead(sensR_b1);
   stateR_b1 = curR_b1 + 2*preR_b1;
   preR_b1 = curR_b1;
-  Serial.print("state R b1 = ");
-  Serial.println(stateR_b1);
   
   curL_b1 = digitalRead(sensL_b1);
   stateL_b1 = curL_b1 + 2*preL_b1;
   preL_b1 = curL_b1;
-  Serial.print("state L b1 = ");
-  Serial.println(stateL_b1);
+  
+  if(!(trainInL_b1 || trainInR_b1)) {
 
-if(!fade_b1) {
 // Is the train entering?
-  if((stateR_b1 == kSet && stateL_b1 != kOn) ||
-     (stateR_b1 != kOn  && stateL_b1 == kSet))
-    fade_b1 = 1;
-
+    trainInR_b1 = stateR_b1 == kSet && stateL_b1 == kOff;
+    trainInL_b1 = stateR_b1 == kOff && stateL_b1 == kSet;
+    if(trainInR_b1 || trainInL_b1)
+      fade_b1 = 1;
+  } else {
+  
 // Is the train exiting?
-  else
-    if((stateR_b1 == kClear && stateL_b1 == kOff) ||
-     (stateR_b1 == kOff   && stateL_b1 == kClear))
-    fade_b1 = -1;
+    trainInR_b1 = !(stateR_b1 == kOff && stateL_b1 == kClear);
+    trainInL_b1 = !(stateR_b1 == kClear && stateL_b1 == kOff);
+    if(!(trainInL_b1 || trainInR_b1)) 
+      fade_b1 = -1;
+  }
 
-// The train is either in or out
-//    else
-//      fade_b1 = 0;
-}
   if(fade_b1 != 0) {
-      
-    if(millis()-fademils_b1 > 50) {
+    
+    if(millis()-fademils_b1 > 5) {
       fademils_b1 = millis();
-
+      
       // change the brightness for next time through the loop:
       brightness_b1 = brightness_b1 + fade_b1 * fadeAmount;
-
+      
       // set the brightness bridge lights
       analogWrite(lights_b1, brightness_b1);
       
       // stop fading if we have reached max or min
       if(brightness_b1 <= 0) {
-	      brightness_b1 = 0;
-	      fade_b1 = 0;
+	brightness_b1 = 0;
+	fade_b1 = 0;
       } else if(brightness_b1 >= 255) {
-	      brightness_b1 = 255;
-//	      fade_b1 = 0;
+	brightness_b1 = 255;
+	fade_b1 = 0;
       }
-   }
-   if(brightness_b1 > 0) {
-     if(millis() - blinkred_b1 > 500) {
-       blinkred_b1 = millis();
-       highlow_b1 = 1-highlow_b1;
-       digitalWrite(redR_b1,highlow_b1);
-       digitalWrite(redL_b1,1-highlow_b1);
     }
-  } 
-  }else {
+  }
+  if(brightness_b1 > 0) {
+    if(millis() - blinkred_b1 > 500) {
+      blinkred_b1 = millis();
+      highlow_b1 = 1-highlow_b1;
+      digitalWrite(redR_b1,highlow_b1);
+      digitalWrite(redL_b1,1-highlow_b1);
+    }
+  } else {
     digitalWrite(redR_b1,HIGH);
     analogWrite(lights_b1, 255);
     digitalWrite(redL_b1,HIGH);
